257aefab62cc016b6dc7be20808b59f75030b9ef
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-19 18:27:25 +0300

    Servis oluşturma ve testte kullanma

--- test/service/map_test.dart
+++ test/service/map_test.dart
@@ -1,15 +1,19 @@
 import 'package:flutter_state_management/feature/maps/model/map_model.dart';
+import 'package:flutter_state_management/feature/maps/service/map_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vexana/vexana.dart';
 
 void main() {
   late INetworkManager networkManager;
+  late IMapService mapService;
   setUp(() {
     networkManager = NetworkManager(options: BaseOptions(baseUrl: 'https://fluttertr-ead5c.firebaseio.com/'));
+    mapService = MapService(networkManager);
   });
   test('Fetch all map datas', () async {
     final response = await networkManager.send<MapModel, List<MapModel>>('maps.json', parseModel: MapModel(), method: RequestType.GET);
-
+    final responseMapService = await mapService.fetchMapItems();
     expect(response.data, isNotNull);
+    expect(responseMapService, isNotNull);
   });
 }
